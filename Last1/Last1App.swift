7f53b2a13ee3db55a7f47a928a6a93b065d86733
Uviation123 <uviation123@outlook.com>
2026-02-22 00:20:16 -0600

    Initial Commit

---
+++ Last1/Last1App.swift
@@ -0,0 +1,17 @@
+//
+//  Last1App.swift
+//  Last1
+//
+//  Created by Samuel Keke on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Last1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Last1Tests/Last1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Last1Tests.swift
+//  Last1Tests
+//
+//  Created by Samuel Keke on 2/22/26.
+//
+
+import Testing
+@testable import Last1
+
+struct Last1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
